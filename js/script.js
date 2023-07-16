50964fd5e7a7e1c6eddee238705e7a3ebd0fd977
Sarvinoz Xushvaqova <132272733+KhushvakovaSarvinoz@users.noreply.github.com>
2023-07-16 10:04:32 -0600

    +

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const canvas = document.querySelector('canvas')
+
+let ctx = canvas.getContext('2d'),
+   isDrawing = false
+
+window.addEventListener('load', ()=>{
+    canvas.width = canvas.offsetWidth
+    canvas.height = canvas.offsetHeight
+})
+
+const startDraw = ()=>{
+    isDrawing = true
+}
+
+const drawing = e => {
+    if (!isDrawing) return
+    ctx.lineTo(e.offsetX, e.offsetY)
+    ctx.stroke()
+}
+const stopDraw = () =>{
+    isDrawing = false
+}
+canvas.addEventListener('mousedown', startDraw)
+canvas.addEventListener('mousemove', drawing)
+canvas.addEventListener('mouseup', stopDraw)
