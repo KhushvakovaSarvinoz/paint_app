76f8eb7f88b4dc25ec6b91309a7d6f4d68ef3012
Sarvinoz Xushvaqova <132272733+KhushvakovaSarvinoz@users.noreply.github.com>
2023-07-17 11:42:10 -0600

    +

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
+//Global variable
 const canvas = document.querySelector('canvas'),
     toolBtns = document.querySelectorAll('.tool')
+
+//Variables
 
 let ctx = canvas.getContext('2d'),
    isDrawing = false,
    brushWidth = 5,
    selectedTool = brush
 
+
+// Set CANVAS width and height
 window.addEventListener('load', ()=>{
     canvas.width = canvas.offsetWidth
     canvas.height = canvas.offsetHeight
 })
 
+// Start drawing
 const startDraw = ()=>{
     isDrawing = true
     ctx.beginPath()
@@ -19,9 +25,19 @@ const startDraw = ()=>{
 
 const drawing = e => {
     if (!isDrawing) return
-    ctx.lineTo(e.offsetX, e.offsetY)
-    ctx.stroke()
+    switch(selectedTool){
+        case 'brush':
+            ctx.lineTo(e.offsetX, e.offsetY)
+            ctx.stroke()
+            break
+        case 'rectangle':
+            drawRectangle()
+            break
+    }
+    
 }
+
+// Tools btn and set variable selected tool
 toolBtns.forEach(btn =>{
     btn.addEventListener('click', () =>{
         document.querySelector('.options .active').classList.remove('active')
@@ -30,6 +46,8 @@ toolBtns.forEach(btn =>{
         console.log(`selected tool ${selectedTool}`)
     })
 })
+
+//Stop drawing
 const stopDraw = () =>{
     isDrawing = false
 }
