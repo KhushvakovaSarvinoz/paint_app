1b93b9fa722512d90a8434170c23ba41a2af1fcc
Sarvinoz Xushvaqova <132272733+KhushvakovaSarvinoz@users.noreply.github.com>
2023-07-20 12:21:22 -0600

    +

--- js/script.js
+++ js/script.js
@@ -1,37 +1,56 @@
-//Global variable
-const canvas = document.querySelector('canvas'),
-    toolBtns = document.querySelectorAll('.tool')
-
 //Variables
+const canvas = document.querySelector('canvas'),
+    toolBtns = document.querySelectorAll('.tool'),
+    fillColor = document.querySelector('#fill-color')
 
 let ctx = canvas.getContext('2d'),
    isDrawing = false,
    brushWidth = 5,
-   selectedTool = brush
+   selectedTool = 'brush',
+   prevMouseX,
+   prevMouseY,
+   snapshot
 
 
 // Set CANVAS width and height
-window.addEventListener('load', ()=>{
+window.addEventListener('load', ()=> { 
     canvas.width = canvas.offsetWidth
     canvas.height = canvas.offsetHeight
 })
 
 // Start drawing
-const startDraw = ()=>{
+const startDraw = e =>{
     isDrawing = true
+    prevMouseX = e.offsetX
+    prevMouseY = e.offsetY
     ctx.beginPath()
     ctx.lineWidth = brushWidth
+    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    console.log(snapshot)
 }
+const drawRectangle = e => {
+    if (!fillColor.checked){
+        ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY) 
+    }else{
+        ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY)  
+    }
+   
+}
+
 
 const drawing = e => {
     if (!isDrawing) return
-    switch(selectedTool){
+    ctx.putImageData(snapshot, 0, 0)
+
+    switch(selectedTool) {
         case 'brush':
             ctx.lineTo(e.offsetX, e.offsetY)
             ctx.stroke()
             break
         case 'rectangle':
-            drawRectangle()
+            drawRectangle(e)
+            break
+        default:
             break
     }
     
